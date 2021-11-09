e504fadcb0663039c94905d176c5f46d01872b07
miraybuy <miraybuyukkaray@gmail.com>
2021-11-09 18:58:52 +0100

    some fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import WeatherInfo from "./WeatherInfo";
+import Weather from "./Weather";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
     <div className="App">
-      <WeatherInfo defaultCity="Milan" />
+      <div className="container">
+        <div className="appWrapper">
+          <div className="weatherApp">
+            <Weather defaultCity="Milan" />
+          </div>
+        </div>
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://github.com/miraybuy"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Miray Buyukkaray
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/miraybuy/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
