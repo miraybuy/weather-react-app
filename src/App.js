38a99f4135c707633c554b171f4c901229616bc3
miraybuy <miraybuyukkaray@gmail.com>
2021-11-09 19:05:49 +0100

    font-size fixes

--- src/App.js
+++ src/App.js
@@ -9,25 +9,25 @@ export default function App() {
           <div className="weatherApp">
             <Weather defaultCity="Milan" />
           </div>
+          <footer>
+            This project is coded by{" "}
+            <a
+              href="https://github.com/miraybuy"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Miray Buyukkaray
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/miraybuy/weather-react-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on Github
+            </a>
+          </footer>
         </div>
-        <footer>
-          This project is coded by{" "}
-          <a
-            href="https://github.com/miraybuy"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Miray Buyukkaray
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/miraybuy/weather-react-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on Github
-          </a>
-        </footer>
       </div>
     </div>
   );
