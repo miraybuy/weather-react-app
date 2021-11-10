ba3296002ae0401eeceb1f2fb23c2bea8fb690d6
miraybuy <miraybuyukkaray@gmail.com>
2021-11-10 16:33:34 +0100

    bug fix

--- src/UnitConversion.js
+++ src/UnitConversion.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export default function UnitConversion(props) {
-  const [unit, setUnit] = useState("celcius");
+  const [unit, setUnit] = useState("celsius");
   function showFahrenheit(event) {
     event.preventDefault();
     setUnit("fahrenheit");
